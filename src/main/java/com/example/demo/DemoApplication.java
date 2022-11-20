8dc79d869bd4a3ffc11f20f7e7785d3247c7b503
Bharath K3 <bharath.k3@ibm.com>
2022-11-20 18:35:52 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello, This is my first application deployed in openshift container";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
